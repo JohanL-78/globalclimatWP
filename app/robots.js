83e6ba988916b4f072c1ce641c0483c6f8214e47
Johan Lorck <kfj.lorck@orange.fr>
2026-03-18 13:37:50 +0100

    bots

--- app/robots.js
+++ app/robots.js
@@ -1,6 +1,27 @@
 export default function robots() {
   return {
-    rules: { userAgent: "*", allow: "/" },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: [
+          "Applebot",
+          "Baiduspider",
+          "YandexBot",
+          "Barkrowler",
+          "AhrefsBot",
+          "PetalBot",
+          "meta-externalagent",
+          "ClaudeBot",
+          "anthropic-ai",
+          "GPTBot",
+          "CCBot",
+        ],
+        disallow: "/",
+      },
+    ],
     sitemap: "https://global-climat.com/sitemap.xml",
   };
 }
